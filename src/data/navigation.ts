86cc0be3303486e3aca68cc6752f9e301bc73908
Patrick Farah <133094582+Patrick-Farah@users.noreply.github.com>
2026-04-25 18:53:43 +0300

    Add site navigation

---
+++ src/data/navigation.ts
@@ -0,0 +1,10 @@
+export const navigation = [
+  { label: 'Home', href: '/' },
+  { label: 'About', href: '/about/' },
+  { label: 'Products', href: '/products/' },
+  { label: 'Solutions', href: '/solutions/' },
+  { label: 'Projects', href: '/projects/' },
+  { label: 'Certifications', href: '/certifications/' },
+  { label: 'Downloads', href: '/downloads/' },
+  { label: 'Contact', href: '/contact/' }
+];
